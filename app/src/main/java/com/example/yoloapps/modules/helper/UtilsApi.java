b0561b2abb54ca5838841b03d531f99cbce51881
Andro Aprila <androaprila12@gmail.com>
2022-08-25 13:50:37 +0700

    IP Changes

--- app/src/main/java/com/example/yoloapps/modules/helper/UtilsApi.java
+++ app/src/main/java/com/example/yoloapps/modules/helper/UtilsApi.java
@@ -1,8 +1,8 @@
 package com.example.yoloapps.modules.helper;
 
 public class UtilsApi {
-    //public static final String BASE_URL_API="http://192.168.18.4:5000/api/";
-    public static final String BASE_URL_API="http://13.213.63.216:80/api/";
+    public static final String BASE_URL_API="http://192.168.18.4:5000/api/";
+    //public static final String BASE_URL_API="http://13.213.63.216:80/api/";
     //public static final String BASE_URL_API="http://9f1b-182-253-116-239.ngrok.io";
 
     public static ApiService getAPIService(){
